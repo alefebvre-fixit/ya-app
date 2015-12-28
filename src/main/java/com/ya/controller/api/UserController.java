b76af8bf4e7f40242598a13b80d803fc965d8378
Antoine Lefebvre <antoine_lefebvre@calypso.com>
2015-12-28 11:17:52 -0800

    update user controller

--- src/main/java/com/ya/controller/api/UserController.java
+++ src/main/java/com/ya/controller/api/UserController.java
@@ -8,64 +8,99 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ya.model.survey.Survey;
+import com.ya.model.user.Profile;
+import com.ya.model.user.YaUser;
 import com.ya.util.Logger;
 
 @RestController
 public class UserController extends YaController {
 
-	@RequestMapping(value = "/api/surveys/{surveyId}", method = RequestMethod.PUT)
-	public Survey update(@PathVariable String surveyId,
-			@RequestBody Survey survey) {
-		Logger.debug("SurveyAPIController.update()");
+	@RequestMapping("/api/me")
+	public YaUser me() {
+		Logger.debug("UserAPIController.me");
 
-		Survey original = getSurveyService().getSurvey(surveyId);
+		return getUserService().findOne(getUserName());
 
-		if (original != null && !original.canUpdate(getUserName())) {
-			// return forbidden();
-			// TODO To be implemented
+	}
+
+	@RequestMapping("/api/users")
+	public List<YaUser> users() {
+		Logger.debug("UserAPIController.users()");
+		return getUserService().findAll();
+	}
+
+	@RequestMapping("/api/users/discovery")
+	public List<YaUser> userDiscovery() {
+		Logger.debug("UserAPIController.accounts()");
+		return getUserService().findAll();
+	}
+
+	@RequestMapping("/api/users/{username}")
+	public YaUser user(@PathVariable String username) {
+		Logger.debug("UserAPIController.user(username)");
+		return getUserService().findOne(username);
+	}
+
+	@RequestMapping(value = "/api/profile", method = RequestMethod.POST)
+	public YaUser updateProfile(@RequestBody Profile profile) {
+		Logger.debug("UserAPIController.updateProfile()");
+
+		YaUser user = getUser();
+		user.setProfile(profile);
+		getUserService().save(user);
+
+		return user;
+
+	}
+
+	@RequestMapping(value = "/api/users/{followee}/follow", method = RequestMethod.POST)
+	public void follow(@PathVariable String followee) {
+		Logger.debug("UserAPIController.follow(followee)" + followee);
+
+		if (!getUserName().equals(followee)) {
+			getUserService().follow(getUserName(), followee);
 		}
 
-		survey.setUsername(getUserName());
-		survey.setId(surveyId);
-		return getSurveyService().save(survey);
-
 	}
 
-	@RequestMapping(value = "/api/survey", method = RequestMethod.POST)
-	public Survey create(@RequestBody Survey survey) {
-		Logger.debug("SurveyAPIController.save()");
+	@RequestMapping(value = "/api/users/{followee}/unfollow", method = RequestMethod.POST)
+	public void unFollow(@PathVariable String followee) {
+		Logger.debug("UserAPIController.unFollow(followee)" + followee);
 
-		survey.setUsername(getUserName());
-		survey.setId(null);
-
-		return getSurveyService().save(survey);
-
-	}
-
-	@RequestMapping("/api/surveys/{surveyId}")
-	public Survey survey(@PathVariable String surveyId) {
-		Logger.debug("SurveyAPIController.survey surveyId =" + surveyId);
-		return getSurveyService().getSurvey(surveyId);
-	}
-
-	@RequestMapping(value = "/api/surveys/{surveyId}", method = RequestMethod.DELETE)
-	public void deleteSurvey(String surveyId) {
-
-		Survey original = getSurveyService().getSurvey(surveyId);
-		if (original != null && !original.canUpdate(getUserName())) {
-			// return forbidden();
-			// TODO
+		if (!getUserName().equals(followee)) {
+			getUserService().unFollow(getUserName(), followee);
 		}
-
-		getSurveyService().delete(surveyId);
 	}
 
-	@RequestMapping("/api/surveys")
-	public List<Survey> surveys() {
-		Logger.debug("SurveyAPIController.surveys()");
+	@RequestMapping("/api/users/{username}/followers")
+	public List<YaUser> followers(@PathVariable String username) {
+		Logger.debug("UserAPIController.followers(username)" + username);
 
-		return getSurveyService().getAll();
+		return getUserService().getFollowers(username);
+	}
+
+	@RequestMapping("/api/users/{username}/followers/size")
+	public int followersSize(@PathVariable String username) {
+		Logger.debug("UserAPIController.followerSize username =" + username);
+		return getUserService().countFollowers(username);
+	}
+
+	@RequestMapping("/api/users/{username}/following")
+	public List<YaUser> following(@PathVariable String username) {
+		Logger.debug("UserAPIController.following username =" + username);
+		return getUserService().findFollowing(username);
+	}
+
+	@RequestMapping("/api/users/{username}/following/names")
+	public List<String> followingNames(@PathVariable String username) {
+		Logger.debug("UserAPIController.followingNames username =" + username);
+		return getUserService().findFollowingNames(username);
+	}
+
+	@RequestMapping("/api/users/{username}/following/size")
+	public int followingSize(String username) {
+		Logger.debug("UserAPIController.followingSize username =" + username);
+		return getUserService().countFollowing(username);
 	}
 
 }
